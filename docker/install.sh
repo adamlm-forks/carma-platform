831211db7ab93dfd6fc89bb692cc3b0a1d6c4cdc
Adam Morrissett <adam.morrissett.ctr@dot.gov>
2021-07-29 13:52:52 -0400

    1374 - Update docker scripts for C1T
    The checkout and install scripts needed to be modified to align with the
    needs of C1T vehicles.
    
    checkout.bash
    * remove existing git clone entries because they are being replaced with a vcs
      import command
    * add vcs import command that pulls from carma-platform.repos
    * update copyright to FHWA
    
    install.bash
    * change build and install steps to build only the necessary packages for a C1T
      deployment. This list will probably be extended as more functionality is
      added to the C1T version

--- docker/install.sh
+++ docker/install.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
 
-#  Copyright (C) 2018-2020 LEIDOS.
-# 
-#  Licensed under the Apache License, Version 2.0 (the "License"); you may not
-#  use this file except in compliance with the License. You may obtain a copy of
-#  the License at
-# 
-#  http://www.apache.org/licenses/LICENSE-2.0
-# 
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#  License for the specific language governing permissions and limitations under
-#  the License.
-
+# Copyright 2021 U.S. Department of Transportation, Federal Highway Administration
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Build the software and its dependencies
 
 set -ex
 
-# Source the autoware installation
-source /opt/autoware.ai/ros/install/setup.bash --extend
+# Source the ROS installation
+source /opt/ros/noetic/setup.bash
 
 cd ~/carma_ws
 
-sudo mkdir /opt/carma # Create install directory
-sudo chown carma /opt/carma # Set owner to expose permissions for build
-sudo chgrp carma /opt/carma # Set group to expose permissions for build
+echo "Building CARMA 1Tenth (C1T)"
 
-echo "Building CARMA"
-
-colcon build --cmake-args --install --install-space /opt/carma/install -DCMAKE_BUILD_TYPE=Release
+catkin_make_isolated --install --install-space /opt/carma/install --only-pkg-with-deps \
+    carma_1tenth \
+    arbitrator \
+    guidance \
+    health_monitor \
+    plan_delegator \
+    trajectory_executor \
+    rosbridge_server \
+    route \
+    -DCMAKE_BUILD_TYPE=Release
 
 echo "Build Complete"
